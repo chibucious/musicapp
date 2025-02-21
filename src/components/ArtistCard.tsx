3b25c3bb23f4e2f6e022f4e12c73a1b367c6f47d
Chibucious <ezukachibueze@gmail.com>
2025-02-21 01:29:00 +0100

    feat: Top Charts & Artsits Page, Updated Tailwid config

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -1,7 +1,29 @@
-const ArtistCard = () => {
-  return (
-    <div>ArtistCard</div>
-  )
+import { ArtistT } from "@/types";
+import { useNavigate } from "react-router-dom";
+
+interface ArtistCardProps {
+  artist: ArtistT;
 }
 
-export default ArtistCard
+const ArtistCard = ({ artist }: ArtistCardProps ) => {
+  const navigate = useNavigate();
+
+  return (
+    <div 
+      className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 
+      backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+      onClick={() => navigate(`/artists/${artist.id}`)}
+    >
+      <img 
+        alt={artist.name} 
+        src={artist.image} 
+        className="w-full h-56 object-cover rounded-lg"
+      />
+      <p className="mt-4 text-white text-lg font-semibold text-center truncate">
+        {artist.name}
+      </p>
+    </div>
+  );
+};
+
+export default ArtistCard;
